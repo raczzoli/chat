2dfe8af76ad0a76f12984ccb89754b646d75538e
Zoltan Racz <racz.zoli@gmail.com>
2025-06-30 17:40:50 +0300

    Removed for now signal(SIGPIPE, SIG_IGN) from main

--- server/main.c
+++ server/main.c
@@ -12,16 +12,7 @@ struct ws_server_config ws_config;
 
 int main()
 {
-	/*
-	 * TODO: we add this here because our platform being multithreaded
-	 * it might write to a socket which was just recently closed, we 
-	 * don`t want our service to crash. But needs to be done using mutexes 
-	 * (or something else) i guess
-	 *
-	 * xxx - solved with mutexes around writes and free-s (needs to be tested)
-	 * ....not working fuck
-	 */
-	signal(SIGPIPE, SIG_IGN); 
+	//signal(SIGPIPE, SIG_IGN); 
 
 	ws_config.port = WSS_PORT;
 	ws_config.ssl_cert_path = WSS_CERT_PATH;
