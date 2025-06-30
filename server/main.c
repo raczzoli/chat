9465bf1dee15a7ace490b411582601d3363bb896
Zoltan Racz <racz.zoli@gmail.com>
2025-06-30 15:42:41 +0300

    Added signal(SIGPIPE, SIG_IGN) to main.c

--- server/main.c
+++ server/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <jansson.h>
+#include <signal.h>
 
 #include "r89.h"
 #include "chat.h"
@@ -11,6 +12,14 @@ struct ws_server_config ws_config;
 
 int main()
 {
+	/*
+	 * TODO: we add this here because our platform being multithreaded
+	 * it might write to a socket which was just recently closed, we 
+	 * don`t want our service to crash. But needs to be done using mutexes 
+	 * (or something else) i guess
+	 */
+	signal(SIGPIPE, SIG_IGN); 
+
 	ws_config.port = WSS_PORT;
 	ws_config.ssl_cert_path = WSS_CERT_PATH;
 	ws_config.ssl_key_path = WSS_KEY_PATH;
